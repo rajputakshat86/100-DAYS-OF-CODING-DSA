76f85ca3fc8ab3a093abee5d40fdb057db8eb33b
Akshat Rajput :) <akshatrajput86@gmail.com>
2026-02-02 13:57:01 +0530

    Add files via upload

---
+++ Q3.C
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    int pos;
+    printf("Enter position to delete (1-based): ");
+    scanf("%d", &pos);
+
+    for (int i = pos - 1; i < n - 1; i++)
+    {
+        arr[i] = arr[i + 1];
+    }
+
+    printf("Updated array:\n");
+    for (int i = 0; i < n - 1; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
---
+++ Q4.C
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter number of days: ");
+    scanf("%d", &n);
+
+    int prices[n];
+    printf("Enter stock prices:\n");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &prices[i]);
+    }
+
+    int minPrice = prices[0];
+    int maxProfit = 0;
+
+    for (int i = 1; i < n; i++)
+    {
+        if (prices[i] < minPrice)
+        {
+            minPrice = prices[i];
+        }
+        else if (prices[i] - minPrice > maxProfit)
+        {
+            maxProfit = prices[i] - minPrice;
+        }
+    }
+
+    printf("Maximum Profit: %d\n", maxProfit);
+
+    return 0;
+}
