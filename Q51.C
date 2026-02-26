1a349cdc43e197a4e041104773f24c0833d0a251
Akshat Rajput :) <akshatrajput86@gmail.com>
2026-02-26 10:19:55 +0530

    Add files via upload

---
+++ Q51.C
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    struct Node *prev;
+};
+
+int main()
+{
+    int n;
+
+    scanf("%d", &n);
+
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+
+    for (int i = 0; i < n; i++)
+    {
+        int value;
+        scanf("%d", &value);
+
+        newNode = (struct Node *)malloc(sizeof(struct Node));
+        newNode->data = value;
+        newNode->next = NULL;
+        newNode->prev = NULL;
+
+        if (head == NULL)
+        {
+
+            head = newNode;
+            temp = newNode;
+        }
+        else
+        {
+
+            temp->next = newNode;
+            newNode->prev = temp;
+            temp = newNode;
+        }
+    }
+
+    temp = head;
+    while (temp != NULL)
+    {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+
+    return 0;
+}
