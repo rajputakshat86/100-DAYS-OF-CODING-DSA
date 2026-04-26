6d2aee18d387292c51a437ccf28256d141c700cc
Akshat Rajput :) <akshatrajput86@gmail.com>
2026-04-26 11:56:43 +0530

    Add files via upload

---
+++ Q170.C
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int findMin(int nums[], int n)
+{
+    int low = 0, high = n - 1;
+
+    while (low < high)
+    {
+        int mid = (low + high) / 2;
+
+        if (nums[mid] > nums[high])
+            low = mid + 1;
+        else
+            high = mid;
+    }
+
+    return nums[low];
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int nums[n];
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &nums[i]);
+
+    int result = findMin(nums, n);
+
+    printf("%d", result);
+
+    return 0;
+}
